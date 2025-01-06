d9cb819bfe608a7793c70669032613ddee37a855
nitin <nit.verma12@gmail.com>
2025-01-06 08:20:16 -0800

    6th jan

---
+++ src/ex_01_Java_Basics/Keyword.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class Keyword
+{
+    public static void main(String[] args) {
+        // i am  a keyword
+        String learning ="Automation";
+        System.out.println(learning);
+    }
+}
