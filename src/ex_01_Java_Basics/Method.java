1e2568bf7b3fb1cf25df47a643e4320be0490259
nitin <nit.verma12@gmail.com>
2025-01-08 02:28:02 -0800

    8th jan

---
+++ src/ex_01_Java_Basics/Method.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class Method {
+
+    public static void main(String[] args) {
+        {
+            System.out.println("i am  nitin");
+            System.out.println("i am sumit");
+            System.out.println("i am main");
+    }}}
+
