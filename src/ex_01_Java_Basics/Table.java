25d3383228186871a1e073c8d93458b770e1f560
nitin <nit.verma12@gmail.com>
2025-01-12 03:46:25 -0800

    8th jan

---
+++ src/ex_01_Java_Basics/Table.java
@@ -0,0 +1,26 @@
+package ex_01_Java_Basics;
+
+import java.sql.SQLOutput;
+
+public class Table {
+    public static void main(String[] args) {
+
+        int num =9;
+
+       System.out.printf("9*1=%d\n",num*1);
+        System.out.printf("9*2=%d\n",num*2);
+        System.out.printf("9*3=%d\n",num*3);
+        System.out.printf("9*4=%d\n",num*4);
+        System.out.printf("9*5=%d\n",num*5);
+        System.out.printf("9*6=%d\n",num*6);
+        System.out.printf("9*7=%d\n",num*7);
+        System.out.printf("9*8=%d\n",num*8);
+        System.out.printf("9*9=%d\n",num*9);
+        System.out.printf("9*10=%d\n",num*10);
+
+
+        //System.out.println("%d*1=");
+
+    }
+
+}
