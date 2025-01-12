6953c2276aaadd3e4842964e62176d9227caea78
nitin <nit.verma12@gmail.com>
2025-01-12 04:05:39 -0800

    12 jan

---
+++ src/ex_01_Java_Basics/Newtable.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class Newtable {
+    public static void main(String[] args) {
+        int num=9;
+    for(int i=1;i<=10;i++)
+    {
+        int num1 = num * i;
+        System.out.printf("%d*1=%d\n",9,num1);
+
+    }}}
+
+
