fa0e2765ef982511f8f9d9fc768e32c865a2d01a
nitin <nit.verma12@gmail.com>
2025-01-13 09:54:27 -0800

    13 jan

---
+++ src/ex_01_Java_Basics/Labex01.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class Labex01
+
+{
+    public static void main(String[] args) {
+        {
+            System.out.println("Hello world");
+        }
+    }
+}
