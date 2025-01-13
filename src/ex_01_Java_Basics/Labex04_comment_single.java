880b5fc0017bd2d7ee71617e588abf28fd8fb270
nitin <nit.verma12@gmail.com>
2025-01-13 10:15:42 -0800

    13 jan

---
+++ src/ex_01_Java_Basics/Labex04_comment_single.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class Labex04_comment_single {
+
+    public static void main(String[] args) {
+        //execute this
+        // and this
+        System.out.println("yes");
+    }
+}
