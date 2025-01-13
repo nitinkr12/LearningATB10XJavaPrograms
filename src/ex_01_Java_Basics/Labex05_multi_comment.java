477ec825883c78c71ed569732d08efc8149edb5d
nitin <nit.verma12@gmail.com>
2025-01-13 10:54:28 -0800

    13 jan

---
+++ src/ex_01_Java_Basics/Labex05_multi_comment.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class Labex05_multi_comment {
+    public static void main(String[] args) {
+        /* im doing
+        very good
+         */
+        System.out.println("multiple");
+    }
+}
