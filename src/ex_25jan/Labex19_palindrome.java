4854befc0419125382ba9383a4c928d02300906d
nitin <nit.verma12@gmail.com>
2025-01-27 04:27:13 -0800

    27 jan

--- src/ex_25jan/Labex19_palindrome.java
+++ src/ex_25jan/Labex19_palindrome.java
@@ -2,7 +2,7 @@ package ex_25jan;
 
 public class Labex19_palindrome {
     public static void main(String[] args) {
-        String Current_str = "naman";
+        String Current_str = "yatin";
         String rev_string = "";
 
         for(int j=0;j<Current_str.length();j++)
@@ -13,6 +13,14 @@ public class Labex19_palindrome {
 
         }
         System.out.println(rev_string);
+        if (Current_str.equals(rev_string))
+         {
+             System.out.println("its a palindrome");
+        }
+        else
+        {
+            System.out.println("it is not palindrome");
+        }
+        }
 
     }
-}
