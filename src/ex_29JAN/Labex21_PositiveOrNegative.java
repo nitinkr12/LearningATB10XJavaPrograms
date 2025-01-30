aa14160862c059139ee9926b702b04264b3b60d8
nitin <nit.verma12@gmail.com>
2025-01-30 03:14:12 -0800

    30 JAN

--- src/ex_29JAN/Labex21_PositiveOrNegative.java
+++ src/ex_29JAN/Labex21_PositiveOrNegative.java
@@ -1,5 +1,6 @@
 package ex_29JAN;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Labex21_PositiveOrNegative {
@@ -7,23 +8,23 @@ public class Labex21_PositiveOrNegative {
     public static void main(String[] args) {
 
 
-       int num;
-       System.out.println("Enter num");
-         Scanner sc = new Scanner(System.in);
-      num= sc.nextInt();
+       Double num;
+       try {
+           System.out.println("Enter num");
+           Scanner sc = new Scanner(System.in);
+           num = sc.nextDouble();
 
-        if (num > 0) {
-            System.out.println("its a positive no");
-        }
-        else if(num < 0)
-        {
-            System.out.println("Its a negative no");
+           if (num > 0) {
+               System.out.println("its a positive no");
+           } else if (num < 0) {
+               System.out.println("Its a negative no");
 
-        } else if (num==0) {
-            System.out.println("its 0");
-        }
-
-        else {
+           } else if (num == 0) {
+               System.out.println("its 0");
+           }
+       }
+        catch(InputMismatchException e)
+       {
             System.out.println("its not a number");
         }
 
