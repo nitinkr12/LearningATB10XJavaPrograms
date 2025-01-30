47d8d773a3af8473b09303527d857c53f1941115
nitin <nit.verma12@gmail.com>
2025-01-30 03:26:00 -0800

    30 JAN

--- src/ex_29JAN/Labex23_max.java
+++ src/ex_29JAN/Labex23_max.java
@@ -1,30 +1,28 @@
 package ex_29JAN;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Labex23_max {
     public static void main(String[] args) {
         int num1;
         int num2;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter your number1");
-        num1= sc.nextInt();
-        System.out.println("Enter your number2");
-        num2= sc.nextInt();
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your number1");
+            num1 = sc.nextInt();
+            System.out.println("Enter your number2");
+            num2 = sc.nextInt();
 
-        if(num1>num2)
-        {
-            System.out.println(num1 + " is max");
+            if (num1 > num2) {
+                System.out.println(num1 + " is max");
+            } else if (num2 > num1) {
+                System.out.println(num2 + " is max");
+            } else if (num1 == num2) {
+                System.out.println("num1 and num2 is equal");
+            }
         }
-        else if(num2>num1)
-        {
-            System.out.println(num2 + " is max");
-        }
-        else if(num1==num2)
-        { System.out.println("num1 and num2 is equal");
-        }
-
-        else {
+        catch (InputMismatchException e){
             System.out.println("its not a number");
         }
 
